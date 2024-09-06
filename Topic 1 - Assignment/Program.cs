bdd7a85e9b7a40192a51be39229ecd0734b7a014
jcampeau25 <jdccanuck25@gmail.com>
2024-09-06 08:46:30 -0400

    finished

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(" JJJJJ  CCCC");
+Console.WriteLine("   J   C");
+Console.WriteLine("   J   C");
+Console.WriteLine("J  J   C");
+Console.WriteLine("JJJJ    CCCC");
+
