db381b7cb1dcebf16f0df8311036801c668778d7
jcampeau25 <jdccanuck25@gmail.com>
2024-09-09 09:06:57 -0400

    finished

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Josh Campeau
+
+Console.WriteLine("Hello, Josh");
+Console.WriteLine();
+//This is why comments are important
+Console.WriteLine("Comments are important because they allow you to leave notes for other people reading yur code to make it easier to understand");
+Console.WriteLine();
 Console.WriteLine(" JJJJJ  CCCC");
 Console.WriteLine("   J   C");
 Console.WriteLine("   J   C");
